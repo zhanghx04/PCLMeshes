78f96916d50bd7d6e3a801e2f99f26ff78d88894
zhanghx04 <zhanghx04@gmail.com>
2022-07-11 13:47:43 -0500

    get all of the parameters' value from the input

--- main.cpp
+++ main.cpp
@@ -22,6 +22,11 @@ int main(int argc, char** argv)
   std::string plyFilename = "/home/jaired/Datasets/PointClouds/ABQ-215-1m-Meru3.ply";
   double searchRadius = 3; // 3
   double mu = 2.5; // 2.5
+  int max_near_neighbor = 100;
+  double max_surface_angle = 45;
+  double min_angle = 10;
+  double max_angle = 120;
+  bool normal_consistency = false;
 
   // int a1 = 2;
   // int a2 = 3;
@@ -30,10 +35,18 @@ int main(int argc, char** argv)
     plyFilename = std::string(argv[1]);
   if (argc >= 3)
     searchRadius = std::stod(argv[2]);
-    // searchRadius = std::stod(argv[2]);
   if (argc >= 4)
-    // mu = std::stod(argv[3]);
-    mu = std::stod(argv[3]);
+    mu = std::stod(argv[3])/10.0;
+  if (argc >= 5)
+    max_near_neighbor = std::stoi(argv[4]);
+  if (argc >= 6)
+    max_surface_angle = std::stod(argv[5]);
+  if (argc >= 7)
+    min_angle = std::stod(argv[6]);
+  if (argc >= 8)
+    max_angle = std::stod(argv[7]); 
+  if (argc >= 9)
+    normal_consistency = (std::stod(argv[8]) == 1); 
 
   PCXYZ::Ptr cloud(new PCXYZ);
 
@@ -81,11 +94,11 @@ int main(int argc, char** argv)
 
   // Set "typical" values for parameters
   gp3.setMu(mu);
-  gp3.setMaximumNearestNeighbors (100);
-  gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
-  gp3.setMinimumAngle(M_PI/18); // 10 degrees
-  gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
-  gp3.setNormalConsistency(false);
+  gp3.setMaximumNearestNeighbors (max_near_neighbor);
+  gp3.setMaximumSurfaceAngle(M_PI/180*max_surface_angle); // 45 degrees
+  gp3.setMinimumAngle(M_PI/180*min_angle); // 10 degrees
+  gp3.setMaximumAngle(M_PI/180*max_angle); // 120 degrees
+  gp3.setNormalConsistency(normal_consistency);
 
   // Get result
   std::cout << "Creating mesh...";
@@ -99,7 +112,30 @@ int main(int argc, char** argv)
   // std::string a2_str = std::to_string (180/a2);
   // a2_str.erase ( a2_str.find_last_not_of('.') + 1, std::string::npos );
 
-  std::string output_filename = "pcl_mu" + std::to_string(mu) + "_maxNeighbor" + std::to_string(100) + ".ply" ;
+  std::string a1_str = std::to_string(searchRadius);
+  a1_str.erase( a1_str.find_first_of('.') + 3); 
+  std::string a2_str = std::to_string(mu);
+  a2_str.erase( a2_str.find_first_of('.') + 3); 
+  std::string a3_str = std::to_string(max_near_neighbor);
+  std::string a4_str = std::to_string(max_surface_angle);
+  a4_str.erase( a4_str.find_first_of('.') + 3); 
+  std::string a5_str = std::to_string(min_angle);
+  a5_str.erase( a5_str.find_first_of('.') + 3); 
+  std::string a6_str = std::to_string(max_angle);
+  a6_str.erase( a6_str.find_first_of('.') + 3); 
+
+  std::string filename = plyFilename.substr(2, plyFilename.length() - 6);
+  std::string normalConsis = normal_consistency ? "True" : "False";
+
+  std::string output_filename = "./checkpoint/" + filename +
+                                "_searchR" + a1_str +
+                                "_mu" + a2_str +
+                                "_maxNeighbor" + a3_str + 
+                                "_maxSurface" + a4_str +
+                                "_minAngle" + a5_str +
+                                "_maxAngle" + a6_str +
+                                "_normalConsistency-" + normalConsis + 
+                                ".ply" ;
 
   pcl::io::savePLYFile( output_filename, triangles);
 
